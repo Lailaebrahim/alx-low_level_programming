15011d747280d66a01526d8ead7c9c1c31b33c0a
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-14 19:03:01 -0700

    Task8 Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 48;
+while (i <= 57)
+{/*to print single digit of HEX use a while loop to loop over the ascii
+  *value of digits from 0 to 9 and use putchar to print
+  *digit using it is ascii
+  *corresponding value
+  */
+putchar (i);
+i++;
+}
+i = 97;
+while (i <= 122)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
