2738573fd64387a514ed44564b5ae3a11b9b3a18
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-14 19:14:57 -0700

    Task9 Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 48;
+while (i <= 57)
+{/*to print alphapets use a while loop to loop over the ascii
+  *value of each alphabet and use putchar to print
+  *alpabet using it is ascii
+  *corresponding value
+  */
+putchar (i);
+putchar (',');
+putchar (' ');
+i++;
+}
+putchar ('\n');
+return (0);
+}
