0a1363056d54b1fd39932eb71e1ab83a27fdca0c
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-14 18:29:41 -0700

    Task3 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 97;
+while (i <= 122)
+{/*to print alphapets use a while loop to loop over the ascii
+  *value of each alphabet and use putchar to print
+  *alpabet using it is ascii
+  *corresponding value
+  */
+putchar (i);
+i++;
+}
+i = 65;
+while (i <= 90)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
