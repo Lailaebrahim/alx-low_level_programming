1d35ffe617ca01b42ebcb4fc816fb7a6773375ae
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-14 19:27:01 -0700

    Advanced task0 Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 48;
+int j = 49;
+while (i <= 56)
+{/*to print alphapets use a while loop to loop over the ascii
+  *value of each alphabet and use putchar to print
+  *alpabet using it is ascii
+  *corresponding value
+  *use nested loop one for the first digit and the other for second
+  */
+putchar (i);
+while (j <= 57)
+{
+putchar (i);
+putchar (j);
+if (i < 56)
+{
+putchar (',');
+putchar (' ');
+}
+i++;
+}
+}
+putchar ('\n');
+return (0);
+}
