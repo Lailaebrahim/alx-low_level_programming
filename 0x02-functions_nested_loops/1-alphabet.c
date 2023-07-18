27208503d334aa1fefd4690af49de93df17625be
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-18 00:18:48 -0700

    Task1 I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "1-alphabet.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+int main(void)
+{
+print_alphabet();
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.h
@@ -0,0 +1,19 @@
+/**
+ * print_alphabet - Write a function that prints the alphabet, in lowercase, followed by a new line.
+ * @param: No parameters
+ * Return: No return
+ */
+void print_alphabet(void)
+{
+int i = 97;
+while (i <= 122)
+{   /*to print alphapets use a while loop to loop over the ascii
+     *value of each alphabet and use putchar to print
+     *alpabet using it is ascii
+     *corresponding value
+     */
+putchar (i);
+i++;
+}
+putchar ('\n');
+}
