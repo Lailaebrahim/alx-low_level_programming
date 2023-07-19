c515f7739055ff01593981ff64134fa79d856e96
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-18 17:24:53 -0700

    Task7 There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - is  function that return last digit of a number
+ * @n: int n takes the number to git it's last digit
+ * Return: last digit of a number
+ */
+int print_last_digit(int n)
+{
+int x = (n % 10);
+return (x);
+}
