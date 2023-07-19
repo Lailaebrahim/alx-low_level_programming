81029ebe18d4cc1929965d5b39aaf747acb8ea66
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-19 13:55:47 -0700

    Task11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,27 +7,15 @@
  */
 void print_to_98(int n)
 {
-int i, step;
 if (n <= 98)
 {
-i = n;
-step = 1;
+for (n = n; n <= 97; n++)
+printf("%d, ", n);
 }
 else
 {
-i = n;
-step = -1;
+for (n = n; n > 98; n--)
+printf("%d, ", n);
 }
-while (i != 99)
-{
-if (i != n)
-{
-_putchar(',');
-_putchar(' ');
-}
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-i += step;
-}
-_putchar('\n');
+printf("98\n");
 }
