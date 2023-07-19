8c8785cd28ab9b5ba0d893ae538ac0eadd0558e2
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-18 18:59:15 -0700

    Task9  Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,17 +5,32 @@
 void times_table(void)
 {
 int i, j;
-i = 0;
+i = 0, j = 0;
 while (i <= 9)
 {
-j = 0;
 while (j <= 9)
 {
-_putchar((i *j) +'0');
+if (((i *j) / 10) == 0)
+{
+putchar((i *j) + '0');
+}
+else
+{
+putchar(((i *j) / 10) + '0');
+putchar(((i *j) % 10) + '0');
+}
+if (j != 9)
+{
 _putchar(',');
 _putchar(' ');
+if (((i *j) / 10) == 0)
+{
+_putchar(' ');
+}
+}
 j++;
 }
+j = 0;
 _putchar('\n');
 i++;
 }
