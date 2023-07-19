5e1fb28e810d850970d50e84cc3e0a8cbd592506
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-19 06:33:42 -0700

    Task1 Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -9,8 +8,8 @@ int main(void)
 {
 int i;
 printf("Infinite loop incoming :(\n");
-i = 0;
-/*while (i < 10)*/
+/*i = 0;*/
+while (i < 10)
 {
 putchar(i);
 }
