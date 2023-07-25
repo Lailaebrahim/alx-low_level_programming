925db1339904ceb1158093d254d956a15c763e3a
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-24 17:27:04 -0700

    Task3 I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout.
+ * @str : pointer to a string
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+_putchar (s[i]);
+i++;
+}
+}
