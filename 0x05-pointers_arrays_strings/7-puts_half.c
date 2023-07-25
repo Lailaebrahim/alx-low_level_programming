ac8d6ae4c76dd4a1a590da75c57de2606efdb9d4
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-24 18:26:55 -0700

    Task7 Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,10 +12,10 @@ int i, half_len, len;
 len = 0;
 for (i = 0; str[i] != '\0'; i++)
 len++;
-half_len = (longi / 2);
+half_len = (len / 2);
 if ((len % 2) == 1)
 half_len = ((len + 1) / 2);
 for (i = half_len; str[i] != '\0'; i++)
-_putchar(str[a]);
+_putchar(str[i]);
 _putchar('\n');
 }
