b202a99c73ad021a3b0e2c75223d85f67e1e062f
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-27 14:27:50 -0700

    Task0 strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,32 +6,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i = 0
+int i = 0;
 int j = 0;
 int len1 = 0;
 int len2 = 0;
-char strcat[200];
-while (s1[i] != '\0')
+while (dest[i] != '\0')
 {
 len1++;
 i++;
 }
-while (s2[j] != '\0')
+while (src[j] != '\0')
 {
 len2++;
 j++;
 }
-for (i = 0 ; i < (len1 + len2) ; i++)
+for (i = len1 ; i < (len1 + len2) ; i++)
 {
-if (i < len1)
-{
-strcat[i] = dest[i];
+dest[i] = src[i - (len1)];
 }
-else
-{
-strcat[i] = src[i - (len1)];
-}
-}
-strcat[len1 + len2] = '\0';
-return (strcat);
+dest[len1 + len2] = '\0';
+return (dest);
 }
