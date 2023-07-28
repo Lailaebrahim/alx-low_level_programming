285f0e77121236c399046afa69c2d649cd2f4790
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-07-28 08:49:01 -0700

    Task6 Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,12 +10,13 @@ char *cap_string(char *str)
 int i = 0;
 while (str[i] != '\0')
 {
-if (str[i] == ' ' || str[i] == '\n' ||  str[i] == ',' ||  str[i] == ';'
-||  str[i] == '!' || str[i] == '?' ||  str[i] == '"' ||  str[i] == ')'
-||  str[i] == '(' ||  str[i] == '}' || str[i] == '{' ||  str[i] == '\t')
+if (str[i - 1] == ' ' || str[i - 1] == '\n'|| str[i - 1] == ',' ||  str[i - 1] == ';'
+||  str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||  str[i - 1] == ')'
+||  str[i - 1] == '(' || str[i - 1] == '}' || str[i - 1] == '{' ||  str[i - 1] == '\t'
+|| i == 0)
 {
-if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-str[i + 1] = str[i + 1] - 32;
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - 32;
 }
 else
 {
