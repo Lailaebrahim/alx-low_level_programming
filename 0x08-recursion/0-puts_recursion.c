5328cdebf18a4892927de2b459d53f76d1970b9d
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-08-03 12:43:34 -0700

    Task0 She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,8 +8,8 @@ void _puts_recursion(char *s)
 if (*s == '\0')
 {
 _putchar ('\n');
-return (void);
+return;
 }
 _putchar (*s);
-return (_puts_recursion(*(s + 1)));
+return (_puts_recursion(s + 1));
 }
