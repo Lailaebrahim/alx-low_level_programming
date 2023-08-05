07ad016ca82f1e40aba0d7dc6b6e8d391c247c33
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-08-04 18:46:30 -0700

    Task0 It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,6 @@
 /**
  * main - entry point
- * @argc : number of arguments passed to the program
+ * @argc : vumber of arguments
  * @argv : 1-d array
  * Return: 0 always succes
  */
@@ -9,6 +9,6 @@
 
 int main(int argc, char *argv[])
 {
-printf("This program was called with \"%s\".\n", argv[0]);
-return (0);
+printf("%s\n", argv[0]);
+return (argc);
 }
