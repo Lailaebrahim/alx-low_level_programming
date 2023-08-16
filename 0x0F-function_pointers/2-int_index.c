93f4446166e591ebe9991b1d84c65fcfd030f06f
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-08-16 16:28:43 -0700

    Task2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+unsigned int i;
+int ret = 0;
+if (array == NULL || cmp == NULL || size <= 0)
+return (-1);
+for (i == 0 ; i < size ; i++)
+{
+ret = cmp(array[i]);
+if (ret == 0)
+return (arra[i]);
+}
+}
