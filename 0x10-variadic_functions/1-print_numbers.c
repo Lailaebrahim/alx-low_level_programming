bd4c8990891b35d2a787f6d784befd8f9a9ecb21
Laila Ebrahim <lailaebrahiem108@gmail.com>
2023-08-17 17:43:10 -0700

    Task0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,9 +15,9 @@ unsigned int i;
 va_start(args, n);
 for (i = 0 ; i < n ; i++)
 {
-  printf("%d", va_arg(args, int));
+printf("%d", va_arg(args, int));
 if (separator != NULL && i != (n - 1))
-printf("%s ", separator);
+printf("%s", separator);
 }
 printf("\n");
 va_end(args);
