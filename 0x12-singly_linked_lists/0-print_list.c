f3cd65cd6a87753c3ef1443145349d70f7b2796d
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-08-24 06:24:00 -0700

    upload task0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_list - a function to print value in list
+ * @h: pointer to head of list
+ *Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+unsigned int i = 0;
+size_t nodes_n = 0;
+char *temp = h->str;
+list_t *temp_h = h; 
+if (h == NULL)
+return (0);
+while (temp_h)
+{
+if (temp->str == NULL)
+{
+temp->str = "(nil)";
+temp->len = 0;
+}
+_putchar('[');
+_putchar(h->len);
+_putchar('[');
+_putchar(' ');
+while (temp[i])
+{
+_putchar(temp[i]);
+i++;
+}
+nodes_n++;
+temp_h = temp_h->next;
+}
