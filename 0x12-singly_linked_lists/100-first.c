2aa682c5d4efcef3c030ea3c1aa084e1be52f6f5
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-08-24 09:21:39 -0700

    upload advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * printBeforeMain - Prints a message before the
+ *                   main function is executed.
+ *
+ * Description: This function is a constructor function
+ *              with the `constructor` attribute.
+ *              It will be executed automatically before
+ *              the `main` function is called.
+ *              It prints a message indicating that
+ *              it is printing before `main`.
+ */
+
+
+void printBeforeMain(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
