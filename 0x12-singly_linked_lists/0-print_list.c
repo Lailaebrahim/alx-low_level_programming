107100f1e34f9325006d22a0b7725dc26a02bbe4
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-08-24 07:58:46 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,42 +10,19 @@ size_t print_list(const list_t *h)
 {
 
 size_t nodes_n = 0;
-char *temp;
-unsigned int i = 0;
 
-if (h == NULL)
-return (0);
 while (h)
 {
 if (h->str == NULL)
 {
-_putchar('[');
-_putchar('0');
- _putchar(']');
-_putchar(' ');
-_putchar('(');
-_putchar('n');
-_putchar('i');
-_putchar('l');
-_putchar(')');
+printf("[0] (nil)\n");
 }
 else
 {
-_putchar('[');
-_putchar('0' + h->len);
-_putchar(']');
-_putchar(' ');
-temp = h->str;
-while (temp[i])
-{
-_putchar(temp[i]);
-i++;
+printf("[%u] %s\n", h->len, h->str);
 }
-}
-_putchar('\n');
 nodes_n++;
 h = h->next;
-i = 0;
 }
 return (nodes_n);
 }
