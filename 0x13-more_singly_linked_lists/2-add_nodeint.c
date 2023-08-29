9b04f3befffd8cc31f38719d2d11212b7ece0970
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-08-29 11:23:17 -0700

    upload Task2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 listint_t *new_node = malloc(sizeof(listint_t));
 if (new_node == NULL)
 return (NULL);
-if (head == NULL)
+if (*head == NULL)
 *head = new_node;
 else
 {
