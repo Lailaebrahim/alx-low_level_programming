a3fae9ddfab3fd0b41d987d3d928636232795ee5
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-08-31 14:55:42 -0700

    fix task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,16 +6,17 @@
  */
 void print_binary(unsigned long int n)
 {
-unsigned long int mask = 1 << (sizeof(unsigned long int) * 8 - 1);
+unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
 
 int leading_zeros = 1;
-
+if (n == 0)
+_putchar ('0');
 while (mask > 0)
 {
 if ((n & mask) != 0)
 {
 leading_zeros = 0;
-_putchar ("1");
+_putchar ('1');
 }
 else if (!leading_zeros)
 {
