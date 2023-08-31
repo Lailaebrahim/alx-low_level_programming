27720342e3a67710d179ab5873d8bf848d979a45
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-08-31 15:22:35 -0700

    fix return value in task 3 and 4

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,5 +11,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 if (index > (sizeof(unsigned long int) * 8 - 1))
 return (-1);
 else
-return (((*n) |= (1 << (index))));
+*n |= (1 << (index));
+return (1);
 }
