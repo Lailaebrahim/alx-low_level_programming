17b18d5dec07d9620aaf860cfe5333fa8d7ee890
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-09-07 18:28:04 -0700

    upload task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,11 +8,13 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 int fd, ret, len = 0;
-if (filename == NULL || text_content == NULL)
+if (filename == NULL)
 return (-1);
 fd = open(filename, O_RDWR | O_APPEND);
 if (fd == -1)
 return (-1);
+if (text_content == NULL)
+return (1);
 while (*(text_content + len) != '\0')
 len++;
 ret = write(fd, text_content, len);
