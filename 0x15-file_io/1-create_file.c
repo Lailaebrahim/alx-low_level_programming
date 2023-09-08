46105645cef7a21db2ff5a869bd641de4b494c22
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-09-07 18:10:58 -0700

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-int len = 0, ret;
+int len = 0, ret = 0;
 if (filename == NULL)
 return (-1);
 fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
@@ -16,7 +16,7 @@ if (fd == -1)
 return (-1);
 if (text_content != NULL)
 {
-if (*(text_content + len) != '\0')
+while (*(text_content + len) != '\0')
 len++;
 ret = write(fd, text_content, len);
 }
