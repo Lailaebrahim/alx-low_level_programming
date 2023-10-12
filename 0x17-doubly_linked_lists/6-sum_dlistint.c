0235bf181e437a92abc48e0df579ff6910966371
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-10-12 15:57:27 -0700

    fix

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -5,7 +5,7 @@
  * @head: pointer to head of the list
  * Return: the summation
  **/
-int *sum_dnodeint(dlistint_t *head)
+int sum_dnodeint(dlistint_t *head)
 {
 int sum = 0;
 dlistint_t *temp = NULL;
