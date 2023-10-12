3c427c80e1f2513578dcdce594c0893ce1affc87
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-10-12 15:44:35 -0700

    Task6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dnodeint - calculate sum of values stored in doubly linked list.
+ * @head: pointer to head of the list
+ * Return: the summation
+ **/
+int *sum_dnodeint(dlistint_t *head)
+{
+int sum = 0;
+dlistint_t *temp = NULL;
+if (head == NULL)
+return (0);
+
+temp = head;
+while (temp)
+{
+sum += temp->n;
+temp = temp->next;
+}
+return (sum);
+}
