f9557ad1c675e74a355041933491f6ba4bd74e4a
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-10-12 13:49:14 -0700

    fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,7 +7,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 size_t count_nodes = 0;
-dlistint_t temp = h;
+const dlistint_t *temp = h;
 if (h == NULL)
 return (count_nodes);
 
@@ -15,7 +15,8 @@ while (h->prev != NULL)
 h = h->prev;
 while (temp != NULL)
 {
-count++;
+printf("%d\n", temp->n);
+count_nodes++;
 temp = temp->next;
 }
 return (count_nodes);
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -1,1 +1,2 @@
-readme fila
+readme file
+
