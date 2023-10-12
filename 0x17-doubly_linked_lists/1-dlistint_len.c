6c0b59561ec02bf46be3109f4cc4b44709142cd9
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-10-12 13:58:01 -0700

    Task1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * dlistint_len - function to get len doubly linked list
+ * @h: pointer to head node
+ * Return: Number of node
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t count_nodes = 0;
+const dlistint_t *temp = h;
+if (h == NULL)
+return (count_nodes);
+
+while (h->prev != NULL)
+h = h->prev;
+while (temp != NULL)
+{
+count_nodes++;
+temp = temp->next;
+}
+return (count_nodes);
+}
