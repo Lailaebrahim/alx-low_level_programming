2d21e1937d66e46af7838cbda3ee854d0c35882b
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-10-12 13:38:16 -0700

    Task0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,7 +8,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 size_t count_nodes = 0;
 dlistint_t temp = h;
-while(temp != NULL)
+if (h == NULL)
+return (count_nodes);
+
+while (h->prev != NULL)
+h = h->prev;
+while (temp != NULL)
 {
 count++;
 temp = temp->next;
