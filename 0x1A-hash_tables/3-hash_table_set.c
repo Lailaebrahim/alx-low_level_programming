75c8f3e70a063441594e3e5df118bafa91a14ef9
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-11-03 10:16:39 -0700

    Task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index = 0;
 hash_node_t *new_node = NULL;
-if(ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
+if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 return (0);
 index = key_index(key, ht->size);
 if (ht->array[index] != NULL)
