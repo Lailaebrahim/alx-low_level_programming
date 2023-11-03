9d7515f17347e1f53a9ee77fa550923b0d46dd6c
lailaebrahim <ebrahimlaila815@gmail.com>
2023-11-03 21:50:09 +0200

    Task5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -5,20 +5,28 @@
 */
 void hash_table_print(const hash_table_t *ht)
 {
-unsigned long int i = 0;
+unsigned long int i;
 hash_node_t *temp = NULL;
+unsigned char flag = 0;
 if (ht == NULL)
 return;
 printf("{");
-for (; i < ((ht->size) - 1) ; i++)
+for (i = 0; i < (ht->size); i++)
 {
-printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+if (ht->array[i] != NULL)
+{
+if (flag == 1)
+printf(", ");
 temp = ht->array[i];
 while(temp != NULL)
 {
 printf("'%s': '%s'", temp->key, temp->value);
-temp = temp->next; 
+temp = temp->next;
+if (temp != NULL)
+printf(", ");
+}
+flag = 1;
 }
 }
-printf("}");
+printf("}\n");
 }
