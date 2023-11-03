6d7e3bd7caae7d76414485acd103352158e12fc6
lailaebrahim <ebrahimlaila815@gmail.com>
2023-11-03 18:40:40 +0200

    Task2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -3,14 +3,23 @@
  * hash_djb2 - function that hashes keys using dgb2 algorithim
  * @str: key to be hashed
  * Return: hash code "index" of key given as a paramter
+ * Description: djb2 algorithim takes an input (usually a string)
+ * and processes it to generate a fixed-size output where that the same
+ * input will always produce the same hash value
+ * steps :
+ * 1- Choose an initial value = 5381
+ * 2- iterate on each char by string
+ * 3- Shift accumulated hash code by 5 "equal to multiply by 33"
+ * and add it to hash code
+ * 4- add the char ascii value
 */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 int c;
-unsigned long int hash_code = 5381;
+unsigned long int hash_code = 5831;
 while ((c = *str++))
 {
-hash_code = ((hash_code << 5) + hash_code)+c;
+hash_code = ((hash_code << 5) + hash_code) + c;
 }
 return (hash_code);
 }
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * key_index - Function that return index for which key will be stored at hash table
+ * @key: Key to be indexed
+ * @size: Size of the hash table array to define range of genertaed index
+ * Descritption: Hashing with division
+ * index = (hash code produces by dgb2 algo % size of hash table array)
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+return (hash_djb2(key) % size);
+}
