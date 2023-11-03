dd8d2b30405e5846c13b04e208a4071424c67534
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-11-03 12:09:38 -0700

    Fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,7 +3,8 @@
  * hash_table_get -  function that retrieves a value associated with a key.
  * @ht: pointer to hash table.
  * @key: key to find it's value.
- * Return:the value associated with the element, or NULL if key couldn’t be found.
+ * Return:the value associated with the element,
+ * or NULL if key couldn’t be found.
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
