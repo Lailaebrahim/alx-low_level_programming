b1d28be5c2f3fa3d5f0fbf5b1694630e3d67ef5f
lailaebrahim <ebrahimlaila815@gmail.com>
2023-11-03 17:26:21 +0200

    Task1

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - function that hashes keys using dgb2 algorithim
+ * @str: key to be hashed
+ * Return: hash code "index" of key given as a paramter
+*/
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash_code = 5831;
+int c = 0;
+while ((c = *str++))
+{
+hash_code = ((hash_code << 5) + hash_code) + c;
+}
+return (hash_code);
+}
