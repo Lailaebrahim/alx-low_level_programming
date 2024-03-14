f4be4df082a0b544b957736ff2d8ae9f48babc09
Lailaebrahim <lailaebrahim815@gmail.com>
2024-03-14 15:34:50 -0700

    Fix Header file gaurding issue

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "search_algos.h"
 
 int linear_search(int *array, size_t size, int value)
 {
+size_t i;
 if (array == NULL)
 return (-1);
-for (size_t i = 0; i < size, i++)
+for (i = 0; i < size; i++)
 {
 printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 if (array[i] == value)
